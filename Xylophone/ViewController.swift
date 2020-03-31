7164ffd202735e46e5844c01e7c9280fe9db1bd3
Kourtnie Henault <kourtnie.m.jenkins@exxonmobil.com>
2020-03-31 12:46:19 -0500

    Upload working project

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        simulateButtonPressEffect(sender: sender)
+        
+        if let buttonTitle = sender.currentTitle {
+                playSound(fileName: buttonTitle, fileExtension: "wav")
+            
+        }else {
+            return
+        }
+    }
     
-
+    func playSound(fileName: String, fileExtension: String) {
+        let url = Bundle.main.url(forResource: fileName, withExtension: fileExtension)
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
+    func simulateButtonPressEffect(sender: UIButton){
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+             sender.alpha = 1.0
+         }
+        
+    }
+    
 }
-
